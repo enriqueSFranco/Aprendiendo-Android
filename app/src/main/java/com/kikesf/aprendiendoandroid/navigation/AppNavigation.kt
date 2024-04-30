bcfd3e1ad2abd247fa49cb2f81e5007692d54c7e
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-04-29 19:34:27 -0600

    feat(navigation): add navigation

---
+++ app/src/main/java/com/kikesf/aprendiendoandroid/navigation/AppNavigation.kt
@@ -0,0 +1,19 @@
+package com.kikesf.aprendiendoandroid.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = AppScreens.MarvelApp.route
+    ) {
+        composable(route = AppScreens.MarvelApp.route) {
+
+        }
+    }
+}
