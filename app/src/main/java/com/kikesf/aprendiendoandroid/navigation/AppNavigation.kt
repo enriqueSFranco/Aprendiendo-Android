14092e000bbc604c2ead176fc6d596fbd55a6274
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-05-02 19:05:22 -0600

    feat(navigation): se creo una clase para el manejo de las vistas

--- app/src/main/java/com/kikesf/aprendiendoandroid/navigation/AppNavigation.kt
+++ app/src/main/java/com/kikesf/aprendiendoandroid/navigation/AppNavigation.kt
@@ -4,16 +4,17 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.kikesf.aprendiendoandroid.screens.FirstScreen
 
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
     NavHost(
         navController = navController,
-        startDestination = AppScreens.MarvelApp.route
+        startDestination = AppScreens.FirstScreen.route
     ) {
-        composable(route = AppScreens.MarvelApp.route) {
-
+        composable(route = AppScreens.FirstScreen.route) {
+            FirstScreen()
         }
     }
 }
