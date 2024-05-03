f39f12cdffc3aceab9024c3caa59d151c0e0750e
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-05-02 19:07:18 -0600

    feat(aprendiendoandroid): main activity

--- app/src/main/java/com/kikesf/aprendiendoandroid/MainActivity.kt
+++ app/src/main/java/com/kikesf/aprendiendoandroid/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -25,17 +26,20 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.kikesf.aprendiendoandroid.screens.FirstScreen
 import com.kikesf.aprendiendoandroid.ui.theme.AprendiendoAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -55,7 +59,7 @@ fun MyApp() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            Conversation(messages)
+            FirstScreen()
         }
     }
 }
@@ -160,7 +164,7 @@ val messages = listOf(
 fun PreviewConversation() {
     AprendiendoAndroidTheme {
         Surface {
-            Conversation(messages)
+            FirstScreen()
         }
     }
 }
