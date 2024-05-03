aed4e1a985c9983e83c8e90774a57fb262469dae
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-05-02 19:04:57 -0600

    feat(SuperHeroApp/services): se creo un objeto para Retrofit

--- app/src/main/java/com/kikesf/aprendiendoandroid/marvelApp/services/ApiService.kt
+++ app/src/main/java/com/kikesf/aprendiendoandroid/marvelApp/services/ApiService.kt
@@ -3,16 +3,26 @@ package com.kikesf.aprendiendoandroid.marvelApp.services
 import com.kikesf.aprendiendoandroid.marvelApp.models.SuperHero
 import com.kikesf.aprendiendoandroid.marvelApp.utils.Constants
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
-    @GET("/v1/public/characters?name=thor&ts=1&apikey=1342ee46b3a8207e80b268dc4b8f97a1&hash=e7538208ae63bf14cfdf1c6e4ecded44")
+    @GET("/v1/public/characters")
     suspend fun getSuperheros(
-        @Path("name")name: String,
+        @Query("name")name: String,
         @Query("ts")ts: String = Constants.timestamp,
-        @Query("apiKey")apiKey: String = Constants.PUBLIC_KEY,
+        @Query("apikey")apiKey: String = Constants.PUBLIC_KEY,
         @Query("hash")hash: String = Constants.hash()
     ): Response<SuperHero>
+}
+
+object RetrofitServiceFactory {
+    fun makeRetrofitService(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
