f33558bbf39ac0c22b1f62357ea69a1e4a2bb8db
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-05-02 19:05:27 -0600

    feat(navigation): se creo una clase para el manejo de las vistas

--- app/src/main/java/com/kikesf/aprendiendoandroid/navigation/AppScreens.kt
+++ app/src/main/java/com/kikesf/aprendiendoandroid/navigation/AppScreens.kt
@@ -1,5 +1,6 @@
 package com.kikesf.aprendiendoandroid.navigation
 
 sealed class AppScreens(val route: String) {
+    object FirstScreen: AppScreens("first_screen")
     object MarvelApp: AppScreens("marvel_app")
 }
