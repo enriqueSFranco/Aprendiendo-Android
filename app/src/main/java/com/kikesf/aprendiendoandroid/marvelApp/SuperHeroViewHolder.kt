4fb1eace1a4384fa43eff8875c814dc7192f33d8
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-05-06 19:39:10 -0600

    feat(res/layouts): carga de imagen con picasso

--- app/src/main/java/com/kikesf/aprendiendoandroid/marvelApp/SuperHeroViewHolder.kt
+++ app/src/main/java/com/kikesf/aprendiendoandroid/marvelApp/SuperHeroViewHolder.kt
@@ -4,8 +4,14 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.kikesf.aprendiendoandroid.databinding.ItemSuperHeroBinding
 import com.kikesf.aprendiendoandroid.marvelApp.models.Result
+import com.squareup.picasso.Picasso
 
 class SuperHeroViewHolder(view: View): RecyclerView.ViewHolder(view) {
     private val binding = ItemSuperHeroBinding.bind(view)
-    fun bin(superHero: Result) {}
+    fun bin(superHero: Result) {
+        binding.tvSuperheroName.text = superHero.name
+        val thumbnail = superHero.thumbnail
+        val imageUrl = "${thumbnail.path}.${thumbnail.extension}"
+        Picasso.get().load(imageUrl).into(binding.ivSuperheroImage)
+    }
 }
