af80656b42d8782fed7d8fb1ed1ea0871d3fdb8d
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-05-07 19:18:31 -0600

    recuperar la imagen con picasso

--- app/src/main/java/com/kikesf/aprendiendoandroid/marvelApp/SuperHeroViewHolder.kt
+++ app/src/main/java/com/kikesf/aprendiendoandroid/marvelApp/SuperHeroViewHolder.kt
@@ -1,6 +1,7 @@
 package com.kikesf.aprendiendoandroid.marvelApp
 
 import android.view.View
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.kikesf.aprendiendoandroid.databinding.ItemSuperHeroBinding
 import com.kikesf.aprendiendoandroid.marvelApp.models.Result
@@ -9,9 +10,17 @@ import com.squareup.picasso.Picasso
 class SuperHeroViewHolder(view: View): RecyclerView.ViewHolder(view) {
     private val binding = ItemSuperHeroBinding.bind(view)
     fun bin(superHero: Result) {
-        binding.tvSuperheroName.text = superHero.name
         val thumbnail = superHero.thumbnail
-        val imageUrl = "${thumbnail.path}.${thumbnail.extension}"
-        Picasso.get().load(imageUrl).into(binding.ivSuperheroImage)
+        val imageUrl = "${thumbnail.path.replace("http", "https")}.${thumbnail.extension}"
+
+        binding.ivSuperheroImage.loadImage(imageUrl)
+        binding.tvSuperheroName.text = superHero.name
+        binding.tvDescription.text = superHero.description
+    }
+
+    private fun ImageView.loadImage(url: String) {
+        if (url.isNotBlank()) {
+            Picasso.get().load(url).into(this)
+        }
     }
 }
